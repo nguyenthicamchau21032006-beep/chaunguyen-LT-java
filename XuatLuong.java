dc1d0129b58be07272d56ad159c9633027765f4b
ChauNguyen <nguyenthicamchau21032006@gmail.com>
2026-05-08 09:02:24 +0700

    hoanthanh

---
+++ XuatLuong.java
@@ -0,0 +1,12 @@
+package bai1;
+
+public class XuatLuong {
+    public static void main(String[] args) {
+        // Tạo nhân viên mới để test
+        NhanVien nv = new NhanVien("Nguyễn", "Văn An", 450);
+        
+        System.out.println("Nhân viên: " + nv.getHo() + " " + nv.getTen());
+        System.out.println("Số sản phẩm: " + nv.getSoSP());
+        System.out.println("Lương: " + nv.getLuong());
+    }
+}
